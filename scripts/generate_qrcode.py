7d29124cb8c6951c8166b408218a506766b24418
Sandra Bastos <sandra@vermillion.net.br>
2026-06-20 16:42:43 -0300

    chore(qrcode): altera script e cria novo qr code

--- scripts/generate_qrcode.py
+++ scripts/generate_qrcode.py
@@ -3,6 +3,6 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.colormasks import RadialGradiantColorMask
 
 qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
-qr.add_data('itepifanio.github.io/renata-2026')
+qr.add_data('unzerstort.github.io/renata-2026')
 img = qr.make_image(image_factory=StyledPilImage, embedded_image_path="emoji.jpg",color_mask=RadialGradiantColorMask())
 img.save('qrcode.png')
